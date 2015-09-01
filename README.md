5a5ae5f33c82359f17712b9584229a6a79fa0e32
Remko Tronçon <git@el-tramo.be>
2015-09-01 23:10:15 +0200

    Import

---
+++ README.md
@@ -0,0 +1,17 @@
+# [json-array-streams: Streams for JSON array parsing/stringifying](https://el-tramo.be/json-array-streams)
+
+## Installation
+
+    npm install json-array-streams --save
+
+## Usage
+
+## API
+
+### `jsonArrayStreams.parse()`
+
+Create a parsing stream.
+
+### `jsonArrayStreams.stringify()`
+
+Create a stringifying stream.
---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var JSONParse = require("jsonparse");
+var through = require("through2");
+
+function createParseStream() {
+	var parser = new JSONParse();
+	return through.obj(
+		function (chunk, enc, cb) {
+			var self = this;
+			parser.onValue = function (value) {
+				if (this.stack.length === 1) {
+					self.push(value);
+				}
+			};
+			parser.write(chunk);
+			cb();
+		});
+}
+
+function createStringifyStream() {
+	var first = true;
+	return through.obj(
+		function (value, enc, cb) {
+			if (first) {
+				first = false;
+				this.push("[");
+			}
+			else {
+				this.push(",");
+			}
+			this.push(JSON.stringify(value));
+			cb();
+		}, 
+		function (cb) {
+			this.push("]");
+			cb();
+		});
+}
+
+module.exports = {
+	parse: createParseStream,
+	stringify: createStringifyStream
+};
